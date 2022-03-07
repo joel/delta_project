859aaff90f1abc2b8de8ef90d8926cbba0c5a2e6
Joel Azemar <joel.azemar@gmail.com>
2022-03-07 16:47:24 +0100

    Add User

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  attribute :first_name, :string, default: -> { FFaker::Name.first_name }
+  attribute :last_name, :string, default: -> { FFaker::Name.last_name }
+  attribute :email, :string, default: -> { FFaker::Internet.email }
+  attribute :birthday, :datetime, default: -> { (10..99).to_a.sample.years.ago }
+  attribute :bio, :text, default: -> { FFaker::Lorem.paragraph }
+end
