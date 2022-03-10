a8c1f657e4aff868dc592a80a06998101b8bf3f3
Joel Azemar <joel.azemar@gmail.com>
2022-03-10 13:52:38 +0100

    Add Sqlite

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,11 @@ module Demo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    if ENV["RAILS_LOG_TO_STDOUT"].present?
+      logger           = ActiveSupport::Logger.new(STDOUT)
+      logger.formatter = config.log_formatter
+      config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    end
   end
 end
