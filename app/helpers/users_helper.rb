9b6c2b53219dd9d590de6f997f99c54c29c7794b
Joel Azemar <joel.azemar@gmail.com>
2022-03-12 16:47:42 +0100

    Add Contract Status in View

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,22 @@
 module UsersHelper
+
+  def contract_status(contract)
+    return "N/A" unless contract
+
+    if contract.approved_at
+      "Approved"
+    else
+      "Pending approval"
+    end
+  end
+
+  def contract_icon(contract)
+    return tag.i(class: "bi bi-exclamation-circle") unless contract
+
+    if contract.approved_at
+      tag.i(class: "bi bi-cloud-sun")
+    else
+      tag.i(class: "bi bi-cloud-rain-heavy")
+    end
+  end
 end
