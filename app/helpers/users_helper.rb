159ffc2cbcd3bb5d9e9ed7e2f8f41b83dc52651c
Joel Azemar <joel.azemar@gmail.com>
2022-03-22 12:47:19 +0100

    Add Signature to the user view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -11,6 +11,26 @@ module UsersHelper
     end
   end
 
+  def contract_signature_status(contract)
+    return "N/A" unless contract
+
+    if contract.signed_at
+      "Signed"
+    else
+      "Pending signature"
+    end
+  end
+
+  def contract_signature_icon(contract)
+    return tag.i(class: "bi bi-exclamation-circle") unless contract
+
+    if contract.signed_at
+      tag.i(class: "bi bi-sun")
+    else
+      tag.i(class: "bi bi-cloud-sun")
+    end
+  end
+
   def contract_icon(contract)
     return tag.i(class: "bi bi-exclamation-circle") unless contract
 
